668af77e3860ec1ff580c1fe2de729de37f14f22
Aurora Muca <aurmuc18@htl-shkoder.com>
2025-01-21 09:44:46 +0100

    First Test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -18,20 +18,5 @@ class CalculatorTest {
         assertEquals(7,calculator.add(3,4));
         assertNotEquals(9, calculator.add(5,5));
     }
-    @Test
-    void subtract() {
-        assertEquals(10,calculator.subtract(20,10));
-        assertNotEquals(9, calculator.subtract(20,10));
-    }
-    @Test
-    void multiply() {
-        assertEquals(24,calculator.multiply(6,4));
-        assertNotEquals(9, calculator.multiply(5,5));
-    }
-    @Test
-    void divide() {
-        assertEquals(8,calculator.divide(32,4));
-        assertNotEquals(9, calculator.divide(32,4));
-        assertThrows(DivideByZeroException.class, () -> calculator.divide(12, 0));
-    }
+
 }
